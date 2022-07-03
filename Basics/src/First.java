323e872a2ec24b6ac1a2618818e2230d34dd69fe
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2022-07-04 02:27:02 +0530

    Learning DSA

---
+++ Basics/src/First.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class First
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Pranit.");
+    }
+}
---
+++ Basics/src/Introduction_to_Programming.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Introduction_to_Programming
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
